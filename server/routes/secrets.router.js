21d8354fd98c5eda188e6ef65a852a382e788632
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-06-24 16:16:06 -0500

    fun stuffs

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,38 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if(req.isAuthenticated() && req.user.clearance_level < 6) {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    pool.query(`SELECT * FROM "secret"
+            WHERE "secrecy_level" < 6;`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    }
+    else if (req.isAuthenticated() && req.user.clearance_level < 13) {
+        console.log('req.user:', req.user);
+        pool.query(`SELECT * FROM "secret"
+            WHERE "secrecy_level" < 13;`)
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+    }
+    else if (req.isAuthenticated() && req.user.clearance_level >= 13) {
+        console.log('req.user:', req.user);
+        pool.query(`SELECT * FROM "secret";`)
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+    }
+    else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
